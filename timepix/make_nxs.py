ef9ba3c57a4c650563c7f1a704d7a322171a88fc
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2021-01-26 14:48:35 +0000

    Clean up

--- timepix/make_nxs.py
+++ timepix/make_nxs.py
@@ -7,23 +7,17 @@ import numpy
 from h5py import AttributeManager
 
 
-def _get_attributes(obj, names, values):
+def get_attributes(obj, names, values):
     for n, v in zip(names, values):
         if type(v) is str:
             v = numpy.string_(v)
         AttributeManager.create(obj, name=n, data=v)
 
 
-class CopyNexusStructure(object):
-    """
-    Class to copy nexus tree from one file to another
-    """
-
-
 def copy_nexus_structure(h5_out: h5py.File, h5_in: h5py.File, nxs: h5py.File, img_ref):
     # Create first level with attributes
     nxentry = nxs.create_group("entry")
-    _get_attributes(nxentry, ("NX_class",), ("NXentry",))
+    get_attributes(nxentry, ("NX_class",), ("NXentry",))
 
     # Copy all of the nexus tree as it is except for /entry/data
     for k in h5_in["entry"].keys():
@@ -44,7 +38,7 @@ def copy_nexus_structure(h5_out: h5py.File, h5_in: h5py.File, nxs: h5py.File, im
             continue
         h5_in["entry/data"].copy(k, nxdata)
         _ax = k
-    _get_attributes(nxdata, ("NX_class", "axes", "signal"), ("NXdata", _ax, "data"))
+    get_attributes(nxdata, ("NX_class", "axes", "signal"), ("NXdata", _ax, "data"))
 
     # Add link to data
     data = nxdata.create_group("data")
