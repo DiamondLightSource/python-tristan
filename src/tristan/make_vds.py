0ec7a1932cf3571ffed5d62006f72c801bc0fc54
Ben Williams <benjaminhwilliams@users.noreply.github.com>
2021-03-17 17:54:12 +0000

    Separate generic tools from VDS generation code
    A precursor to a Dask alternative to VDS, reusing much of the VDS code.

--- src/tristan/make_vds.py
+++ src/tristan/make_vds.py
@@ -11,12 +11,14 @@ import argparse
 import re
 import sys
 from contextlib import ExitStack
-from itertools import chain, compress, cycle, filterfalse, zip_longest
+from itertools import compress
 from pathlib import Path
 from typing import Dict, Iterable, List
 
 import h5py
 import numpy as np
+
+from .data import data_files, source_carousel, time_slice_info_from_metadata
 
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument(
@@ -37,13 +39,10 @@ parser.add_argument(
 parser.add_argument(
     "-f",
     "--force",
-    help="Force the output VDS file to over-write any existing file with the same name.",
+    help="Force the output file to over-write any existing file with the same name.",
     action="store_true",
 )
 
-
-TimeSliceInfo = List[slice], np.ndarray, List[slice], List[slice]
-DataInfo = List[Path], *TimeSliceInfo
 Sources = Dict[str, List[h5py.VirtualSource]]
 VirtualSourceInfo = Sources, Sources, List[int], Dict[str, type]
 Layouts = Dict[str, h5py.VirtualLayout]
@@ -51,79 +50,35 @@ Layouts = Dict[str, h5py.VirtualLayout]
 event_keys = "event_energy", "event_id", "event_time_offset"
 cue_keys = "cue_id", "cue_timestamp_zero"
 
-# Regex for the names of data sets, in the time slice metadata file, representing the
-# distribution of time slices across raw data files for each module.
-ts_key_regex = re.compile(r"ts_qty_module\d{2}")
 
+def find_file_names(in_file: str, out_file: str, force: bool) -> (Path, str, Path):
+    """Resolve the input and output file names."""
+    in_file = Path(in_file).expanduser().resolve()
 
-def ts_from_metadata(meta_file: h5py.File) -> TimeSliceInfo:
-    """
-    Assemble information about the event data time slices from the metadata file.
+    data_dir = in_file.parent
 
-    Args:
-        meta_file:  Metadata ('_meta.h5') file.  Assumes metadata version 1.
-
-    Returns:
-        - List of slices with which to divide a lexicographically sorted list of file
-          paths into sub-lists, each corresponding to a different detector module.
-          Ordered by module number.  Length is the number of modules in the detector.
-        - List of the number of events in each time slice, in the order that the time
-          slices will appear in the VDS.  Length is the number of time slices recorded.
-        - List of slice objects, each representing the position of a time slice in
-          the virtual layouts of events data.  Length and order correspond to
-          'events_per_ts'.
-        - List of slice objects used to select each time slice from the virtual source
-          objects in order to populate the virtual layouts.  Length and order
-          correspond to 'events_per_ts'.
-    """
-    fp_per_module = meta_file["fp_per_module"][()]
-
-    ts_keys = sorted(filter(ts_key_regex.match, meta_file.keys()))
-    ts_data = [meta_file[ts_key] for ts_key in ts_keys]
-
-    time_slices = []
-    num_events_per_ts = []
-    # Loop through the modules, acting on the time slice metadata for each in turn.
-    for num_files, ts_counts in zip(fp_per_module, ts_data):
-        ts_counts = ts_counts[()]
-        # Reshape the time slice metadata for a single module into a rectangular array
-        # with shape (number of time slices per file, number of files), so as to be
-        # able to generate file-specific slices.
-        num_ts_per_fp = -(-ts_counts.size // num_files)
-        ts_counts.resize(num_ts_per_fp * num_files)
-        # Keep a separate record of each module's array of event counts per time slice.
-        num_events_per_ts.append(ts_counts)
-        ts_counts = ts_counts.reshape(num_ts_per_fp, num_files)
-        # Generate the cumulative count of events per time slice for each file.
-        ts_per_module = np.pad(np.cumsum(ts_counts, axis=0), ((1, 0), (0, 0)))
-        # Turn these counts into slices to select from a virtual source for each file.
-        time_slices.append(
-            map(slice, ts_per_module[:-1].flatten(), ts_per_module[1:].flatten())
+    # Get the segment 'name_root' from 'name_root_meta.h5' or 'name_root_000001.h5'.
+    file_name_root = re.fullmatch(r"(.*)_(?:meta|\d+)", in_file.stem)
+    if file_name_root:
+        file_name_root = file_name_root[1]
+    else:
+        sys.exit(
+            "Input file name did not have the expected format '<name>_meta.h5':\n"
+            f"\t{in_file}"
         )
 
-    # Assemble all the source slices into a single list, ordered first
-    # chronologically, then by module number.  Where modules have recorded different
-    # numbers of time slices, zip_longest will pad with None.
-    time_slices = list(chain.from_iterable(zip_longest(*time_slices)))
+    if out_file:
+        out_file = Path(out_file).expanduser().resolve()
+    else:
+        out_file = data_dir / (file_name_root + "_vds.h5")
 
-    # Resize each module's array of event counts per time slice so that their sizes
-    # match.  This is achieved by zero-padding to match the None-padding of the list
-    # of time slices by zip_longest.
-    max_size = max(data.size for data in num_events_per_ts)
-    num_events_per_ts = np.column_stack(
-        [np.pad(data, (0, max_size - data.size)) for data in num_events_per_ts]
-    ).flatten()
-    # Compute the cumulative count of events in the order of time slices in the VDS.
-    event_slices = np.pad(num_events_per_ts.cumsum(), (1, 0))
-    # Use this to generate the slices used to assign to the virtual layout.
-    event_slices = list(map(slice, event_slices[:-1], event_slices[1:]))
+    if not force and out_file.exists():
+        sys.exit(
+            f"This output file already exists:\n\t{out_file}\n"
+            f"Use '-f' to override or specify a different output file path with '-o'."
+        )
 
-    # Generate a slice for each module, with which to select the corresponding raw
-    # data files from a lexicographically sorted list.
-    fp_per_module = np.pad(np.cumsum(fp_per_module), (1, 0))
-    fp_per_module = list(map(slice, fp_per_module[:-1], fp_per_module[1:]))
-
-    return fp_per_module, num_events_per_ts, event_slices, time_slices
+    return data_dir, file_name_root, out_file
 
 
 def virtual_sources(files: List[Path]) -> VirtualSourceInfo:
@@ -174,49 +129,9 @@ def virtual_layouts(num_events: int, num_cues: int, dtypes: Dict[str, type]) -> 
     return layouts
 
 
-def data_info(data_dir: Path, root: str, n_dig: int = 6) -> DataInfo:
-    """
-    Extract information about the layout of events data on disk, for creating a VDS.
-
-    Args:
-        data_dir: Directory containing the raw data and time slice metadata HDF5 files.
-        root:     Input file name, stripped of '_meta.h5', '_000001.h5', etc..
-        n_dig:    Number of digits in the raw file number, e.g. six in '_000001.h5'.
-
-    Returns:
-        - Lexicographically sorted list of raw file paths.
-        - List of slices with which to divide the above list of file paths into
-          sub-lists, each corresponding to a different detector module.  Ordered by
-          module number.  Length is the number of modules in the detector.
-        - List of the number of events in each time slice, in the order that the time
-          slices will appear in the VDS.  Length is the number of time slices recorded.
-        - List of slice objects, each representing the position of a time slice in
-          the virtual layouts of events data.  Length and order correspond to
-          'events_per_ts'.
-        - List of slice objects used to select each time slice from the virtual source
-          objects in order to populate the virtual layouts.  Length and order
-          correspond to 'events_per_ts'.
-    """
-    meta_file = data_dir / f"{root}_meta.h5"
-    if not meta_file.exists():
-        sys.exit(f"Could not find the expected detector metadata file:\n\t{meta_file}")
-
-    with h5py.File(meta_file, "r") as f:
-        fp_per_module, events_per_ts, event_slices, time_slices = ts_from_metadata(f)
-
-    num_files = fp_per_module[-1].stop
-    raw_files = [data_dir / f"{root}_{n + 1:0{n_dig}d}.h5" for n in range(num_files)]
-    missing_files = list(filterfalse(Path.exists, raw_files))
-    if missing_files:
-        missing_files = "\n\t".join(map(str, missing_files))
-        sys.exit(f"The following expected data files are missing:\n\t{missing_files}")
-
-    return raw_files, fp_per_module, events_per_ts, event_slices, time_slices
-
-
 def virtual_data_set(
     raw_files: List[Path],
-    fp_per_module: List[slice],
+    file_slices: List[slice],
     events_per_ts: Iterable,
     event_slices: List[slice],
     time_slices: List[slice],
@@ -230,7 +145,7 @@ def virtual_data_set(
 
     Args:
         raw_files:      Lexicographically sorted list of raw file paths.
-        fp_per_module:  List of slices to divide the above list of file paths into
+        file_slices:    List of slices to divide the above list of file paths into
                         sub-lists, each corresponding to a different detector module.
                         Ordered by module number.  Length is the number of modules in
                         the detector.
@@ -252,8 +167,7 @@ def virtual_data_set(
     # In order to assemble the event time slices in the desired order (chronological,
     # then by  module), we need to create a carousel of the event data sources.
     for key in event_keys:
-        carousel = (cycle(event_sources[key][fileslice]) for fileslice in fp_per_module)
-        event_sources[key] = chain.from_iterable(zip(*carousel))
+        event_sources[key] = source_carousel(event_sources[key], file_slices)
 
     # Generate the slices used to assign the cue data to their virtual layouts.
     cue_slices = np.pad(np.cumsum(num_cues_per_file), (1, 0))
@@ -280,44 +194,18 @@ def virtual_data_set(
     return layouts
 
 
-def find_file_names(args: argparse.Namespace) -> (Path, str, Path):
-    """Resolve the input and output file names."""
-    input_file = Path(args.input_file).expanduser().resolve()
-
-    data_dir = input_file.parent
-
-    # Get the segment 'name_root' from 'name_root_meta.h5' or 'name_root_000001.h5'.
-    file_name_root = re.fullmatch(r"(.*)_(?:meta|\d+)", input_file.stem)
-    if file_name_root:
-        file_name_root = file_name_root[1]
-    else:
-        sys.exit(
-            "Input file name did not have the expected format '<name>_meta.h5':\n"
-            f"\t{input_file}"
-        )
-
-    if args.output_file:
-        output_file = Path(args.output_file).expanduser().resolve()
-    else:
-        output_file = data_dir / (file_name_root + "_vds.h5")
-
-    if not args.force and output_file.exists():
-        sys.exit(
-            f"This output file already exists:\n\t{output_file}\n"
-            f"Use '-f' to override or specify a different output file path with '-o'."
-        )
-
-    return data_dir, file_name_root, output_file
-
-
 def main(args=None):
     """Utility for making an HDF5 VDS from raw Tristan data."""
     args = parser.parse_args(args)
-    data_dir, root, output_file = find_file_names(args)
+    data_dir, root, output_file = find_file_names(
+        args.input_file, args.output_file, args.force
+    )
 
-    info = data_info(data_dir, root)
+    raw_files, meta_file = data_files(data_dir, root)
+    with h5py.File(meta_file, "r") as f:
+        time_slice_info = time_slice_info_from_metadata(f)
 
-    layouts = virtual_data_set(*info)
+    layouts = virtual_data_set(raw_files, *time_slice_info)
 
     with h5py.File(output_file, "w" if args.force else "x") as f:
         for layout in layouts.items():
