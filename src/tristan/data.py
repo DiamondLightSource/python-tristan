b8838775c14813cb11a211d8db1202817fec179e
Ben Williams <benjaminhwilliams@users.noreply.github.com>
2021-03-18 19:28:03 +0000

    Another rationalisation of the VDS creation code
    Tidy up the VDS code to rationalise the operation of some functions.
    Return VDS-specific code to the VDS module, since it seems to be of
    little use in constructing the Dask alternative.

--- src/tristan/data.py
+++ src/tristan/data.py
@@ -1,14 +1,12 @@
 """Tools for extracting data on cues and events from Tristan data files."""
 import re
 import sys
-from itertools import chain, cycle, filterfalse, zip_longest
+from itertools import filterfalse
 from pathlib import Path
-from typing import Iterable, Iterator, List, Sequence
+from typing import List
 
 import h5py
 import numpy as np
-
-TimeSliceInfo = List[slice], np.ndarray, List[slice], List[slice]
 
 # Regex for the names of data sets, in the time slice metadata file, representing the
 # distribution of time slices across raw data files for each module.
@@ -41,88 +39,3 @@ def data_files(data_dir: Path, root: str, n_dig: int = 6) -> (List[Path], Path):
         sys.exit(f"The following expected data files are missing:\n\t{missing_files}")
 
     return raw_files, meta_file
-
-
-def time_slice_info_from_metadata(meta_file: h5py.File) -> TimeSliceInfo:
-    """
-    Assemble information about the event data time slices from the metadata file.
-
-    Args:
-        meta_file:  Metadata ('_meta.h5') file.  Assumes metadata version 1.
-
-    Returns:
-        - List of slices with which to divide a lexicographically sorted list of file
-          paths into sub-lists, each corresponding to a different detector module.
-          Ordered by module number.  Length is the number of modules in the detector.
-        - List of the number of events in each time slice, in the order that the time
-          slices will appear in the VDS.  Length is the number of time slices recorded.
-        - List of slice objects, each representing the position of a time slice in
-          the virtual layouts of events data.  Length and order correspond to
-          'events_per_ts'.
-        - List of slice objects used to select each time slice from the virtual source
-          objects in order to populate the virtual layouts.  Length and order
-          correspond to 'events_per_ts'.
-    """
-    fp_per_module = meta_file["fp_per_module"][()]
-
-    ts_keys = sorted(filter(ts_key_regex.match, meta_file.keys()))
-    ts_data = [meta_file[ts_key] for ts_key in ts_keys]
-
-    time_slices = []
-    num_events_per_ts = []
-    # Loop through the modules, acting on the time slice metadata for each in turn.
-    for num_files, ts_counts in zip(fp_per_module, ts_data):
-        ts_counts = ts_counts[()]
-        # Reshape the time slice metadata for a single module into a rectangular array
-        # with shape (number of time slices per file, number of files), so as to be
-        # able to generate file-specific slices.
-        num_ts_per_fp = -(-ts_counts.size // num_files)
-        ts_counts.resize(num_ts_per_fp * num_files)
-        # Keep a separate record of each module's array of event counts per time slice.
-        num_events_per_ts.append(ts_counts)
-        ts_counts = ts_counts.reshape(num_ts_per_fp, num_files)
-        # Generate the cumulative count of events per time slice for each file.
-        ts_per_module = np.pad(np.cumsum(ts_counts, axis=0), ((1, 0), (0, 0)))
-        # Turn these counts into slices to select from a virtual source for each file.
-        time_slices.append(
-            map(slice, ts_per_module[:-1].flatten(), ts_per_module[1:].flatten())
-        )
-
-    # Assemble all the source slices into a single list, ordered first
-    # chronologically, then by module number.  Where modules have recorded different
-    # numbers of time slices, zip_longest will pad with None.
-    time_slices = list(chain.from_iterable(zip_longest(*time_slices)))
-
-    # Resize each module's array of event counts per time slice so that their sizes
-    # match.  This is achieved by zero-padding to match the None-padding of the list
-    # of time slices by zip_longest.
-    max_size = max(data.size for data in num_events_per_ts)
-    num_events_per_ts = np.column_stack(
-        [np.pad(data, (0, max_size - data.size)) for data in num_events_per_ts]
-    ).flatten()
-    # Compute the cumulative count of events in the order of time slices in the VDS.
-    event_slices = np.pad(num_events_per_ts.cumsum(), (1, 0))
-    # Use this to generate the slices used to assign to the virtual layout.
-    event_slices = list(map(slice, event_slices[:-1], event_slices[1:]))
-
-    # Generate a slice for each module, with which to select the corresponding raw
-    # data files from a lexicographically sorted list.
-    file_slices = np.pad(np.cumsum(fp_per_module), (1, 0))
-    file_slices = list(map(slice, file_slices[:-1], file_slices[1:]))
-
-    return file_slices, num_events_per_ts, event_slices, time_slices
-
-
-def source_carousel(sources: Sequence, source_slices: Iterable) -> Iterator:
-    """
-    TODO: Add docstring
-
-    Args:
-        sources:
-        source_slices:
-
-    Returns:
-
-    """
-    carousel = zip(*(cycle(sources[source_slice]) for source_slice in source_slices))
-    return chain.from_iterable(carousel)
--- src/tristan/vds.py
+++ src/tristan/vds.py
@@ -11,7 +11,7 @@ import argparse
 import re
 import sys
 from contextlib import ExitStack
-from itertools import compress
+from itertools import chain, compress, cycle, zip_longest
 from pathlib import Path
 from typing import Dict, Iterable, List
 
@@ -19,7 +19,7 @@ import h5py
 import numpy as np
 
 from . import cue_keys, event_keys
-from .data import data_files, source_carousel, time_slice_info_from_metadata
+from .data import data_files, ts_key_regex
 
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument(
@@ -44,7 +44,8 @@ parser.add_argument(
     action="store_true",
 )
 
-Sources = Dict[str, List[h5py.VirtualSource]]
+Sources = Dict[str, Iterable[h5py.VirtualSource]]
+TimeSliceInfo = List[slice], np.ndarray, List[slice], List[slice]
 VirtualSourceInfo = Sources, Sources, List[int], Dict[str, type]
 Layouts = Dict[str, h5py.VirtualLayout]
 
@@ -79,17 +80,74 @@ def find_file_names(in_file: str, out_file: str, force: bool) -> (Path, str, Pat
     return data_dir, file_name_root, out_file
 
 
-def virtual_sources(files: List[Path]) -> VirtualSourceInfo:
+def time_slice_info(meta_file: h5py.File) -> TimeSliceInfo:
+    """
+    Assemble information about the event data time slices from the metadata file.
+
+    Args:
+        meta_file:  Metadata ('_meta.h5') file.  Assumes metadata version 1.
+
+    Returns:
+        - List of slice objects used to select each time slice from the virtual source
+          objects in order to populate the virtual layouts.  Length and order
+          correspond to 'events_per_ts'.
+        - List of the number of events in each time slice, in the order that the time
+          slices will appear in the VDS.  Length is the number of time slices recorded.
+    """
+    fp_per_module = meta_file["fp_per_module"][()]
+
+    ts_keys = sorted(filter(ts_key_regex.match, meta_file.keys()))
+    ts_data = [meta_file[ts_key] for ts_key in ts_keys]
+
+    time_slices = []
+    num_events_per_ts = []
+    # Loop through the modules, acting on the time slice metadata for each in turn.
+    for num_files, ts_counts in zip(fp_per_module, ts_data):
+        ts_counts = ts_counts[()]
+        # Reshape the time slice metadata for a single module into a rectangular array
+        # with shape (number of time slices per file, number of files), so as to be
+        # able to generate file-specific slices.
+        num_ts_per_fp = -(-ts_counts.size // num_files)
+        ts_counts.resize(num_ts_per_fp * num_files)
+        # Keep a separate record of each module's array of event counts per time slice.
+        num_events_per_ts.append(ts_counts)
+        ts_counts = ts_counts.reshape(num_ts_per_fp, num_files)
+        # Generate the cumulative count of events per time slice for each file.
+        ts_per_module = np.pad(np.cumsum(ts_counts, axis=0), ((1, 0), (0, 0)))
+        # Turn these counts into slices to select from a virtual source for each file.
+        time_slices.append(
+            map(slice, ts_per_module[:-1].flatten(), ts_per_module[1:].flatten())
+        )
+
+    # Assemble all the source slices into a single list, ordered first
+    # chronologically, then by module number.  Where modules have recorded different
+    # numbers of time slices, zip_longest will pad with None.
+    time_slices = list(chain.from_iterable(zip_longest(*time_slices)))
+
+    # Resize each module's array of event counts per time slice so that their sizes
+    # match.  This is achieved by zero-padding to match the None-padding of the list
+    # of time slices by zip_longest.
+    max_size = max(data.size for data in num_events_per_ts)
+    num_events_per_ts = np.column_stack(
+        [np.pad(data, (0, max_size - data.size)) for data in num_events_per_ts]
+    ).flatten()
+
+    return time_slices, num_events_per_ts
+
+
+def virtual_sources(files: List[Path], meta_file: h5py.File) -> VirtualSourceInfo:
     """
     Create HDF5 virtual sources and collate ancillary information from raw data files.
 
     Args:
-        files:  Lexicographically sorted list of raw file paths.
+        files:      Lexicographically sorted list of raw file paths.
+        meta_file:  Tristan detector metadata file object.
 
     Returns:
-        - Dictionary of event data set names and lists of corresponding HDF5 virtual
-          sources.  The lists of sources have length and order as per the list of input
-          files.
+        - Dictionary of event data set names and iterators of corresponding HDF5 virtual
+          sources.  The iterator of sources for each data set is based on
+          itertools.cycle and so repeats indefinitely in the order in which successive
+          event slices should be selected to build the virtual data set.
         - Dictionary of cue data set names and lists of corresponding HDF5 virtual
           sources.  The lists of sources have length and order as per the list of input
           files.
@@ -114,6 +172,19 @@ def virtual_sources(files: List[Path]) -> VirtualSourceInfo:
             for key in cue_keys:
                 cue_sources[key].append(h5py.VirtualSource(raw_file[key]))
 
+    # Make a list of slices with which to divide the lexicographically sorted list of
+    # file paths into sub-lists, each slice corresponding to a different detector
+    # module.  Ordered by module number.  Length is equal to the number of modules in
+    # the detector.
+    file_slices = np.pad(np.cumsum(meta_file["fp_per_module"]), (1, 0))
+    file_slices = list(map(slice, file_slices[:-1], file_slices[1:]))
+
+    # Construct a carousel to select time slices in the order in which they should
+    # appear in the virtual layout.
+    for key, sources in event_sources.items():
+        carousel = zip(*(cycle(sources[file_slice]) for file_slice in file_slices))
+        event_sources[key] = chain.from_iterable(carousel)
+
     return event_sources, cue_sources, num_cues_per_file, dtypes
 
 
@@ -129,10 +200,9 @@ def virtual_layouts(num_events: int, num_cues: int, dtypes: Dict[str, type]) -> 
 
 def virtual_data_set(
     raw_files: List[Path],
-    file_slices: List[slice],
-    events_per_ts: Iterable,
-    event_slices: List[slice],
+    meta_file: h5py.File,
     time_slices: List[slice],
+    events_per_ts: Iterable[int],
 ) -> Layouts:
     """
     Define a virtual data set in the form of virtual layouts linked to virtual sources.
@@ -143,36 +213,31 @@ def virtual_data_set(
 
     Args:
         raw_files:      Lexicographically sorted list of raw file paths.
-        file_slices:    List of slices to divide the above list of file paths into
-                        sub-lists, each corresponding to a different detector module.
-                        Ordered by module number.  Length is the number of modules in
-                        the detector.
-        events_per_ts:  List of the number of events in each time slice, in the order
-                        that the time slices will appear in the VDS.  Length is the
-                        number of time slices recorded.
-        event_slices:   List of slice objects, each representing the position of a time
-                        slice in the virtual layouts of events data.  Length and order
-                        correspond to 'events_per_ts'.
+        meta_file:      Tristan detector metadata file object.
         time_slices:    List of slice objects used to select each time slice from the
                         virtual source objects in order to populate the virtual
                         layouts.  Length and order correspond to 'events_per_ts'.
+        events_per_ts:  List of the number of events in each time slice, in the order
+                        that the time slices will appear in the VDS.  Length is the
+                        number of time slices recorded.
 
     Returns:
         A dictionary of raw data set names and corresponding HDF5 virtual layouts.
     """
-    event_sources, cue_sources, num_cues_per_file, dtypes = virtual_sources(raw_files)
-
-    # In order to assemble the event time slices in the desired order (chronological,
-    # then by  module), we need to create a carousel of the event data sources.
-    for key in event_keys:
-        event_sources[key] = source_carousel(event_sources[key], file_slices)
+    event_sources, cue_sources, cues_per_file, dtypes = virtual_sources(
+        raw_files, meta_file
+    )
 
     # Generate the slices used to assign the cue data to their virtual layouts.
-    cue_slices = np.pad(np.cumsum(num_cues_per_file), (1, 0))
+    cue_slices = np.pad(np.cumsum(cues_per_file), (1, 0))
+    num_cues = cue_slices[-1]
     cue_slices = list(map(slice, cue_slices[:-1], cue_slices[1:]))
 
-    num_events = event_slices[-1].stop
-    num_cues = cue_slices[-1].stop
+    # Generate the slices used to assign the event data to their virtual layouts.
+    event_slices = np.pad(np.cumsum(events_per_ts), (1, 0))
+    num_events = event_slices[-1]
+    event_slices = list(map(slice, event_slices[:-1], event_slices[1:]))
+
     layouts = virtual_layouts(num_events, num_cues, dtypes)
 
     # Map virtual source slices to virtual layout slices.
@@ -186,7 +251,7 @@ def virtual_data_set(
             layout[event_slice] = source[time_slice]
     for key, sources in cue_sources.items():
         layout = layouts[key]
-        for cue_slice, source, count in zip(cue_slices, sources, num_cues_per_file):
+        for cue_slice, source, count in zip(cue_slices, sources, cues_per_file):
             layout[cue_slice] = source[:count]
 
     return layouts
@@ -201,9 +266,8 @@ def main(args=None):
 
     raw_files, meta_file = data_files(data_dir, root)
     with h5py.File(meta_file, "r") as f:
-        time_slice_info = time_slice_info_from_metadata(f)
-
-    layouts = virtual_data_set(raw_files, *time_slice_info)
+        ts_info = time_slice_info(f)
+        layouts = virtual_data_set(raw_files, f, *ts_info)
 
     with h5py.File(output_file, "w" if args.force else "x") as f:
         for layout in layouts.items():
