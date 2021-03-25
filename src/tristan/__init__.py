bac744375b0d347d3c271d36423603c99cc43941
Ben Williams <benjaminhwilliams@users.noreply.github.com>
2021-03-25 17:50:40 +0000

    Add some module-level dunder variables

--- src/tristan/__init__.py
+++ src/tristan/__init__.py
@@ -8,6 +8,11 @@ experimental Timepix-based event-mode detector, codenamed Tristan, at Diamond Li
 Source.
 """
 
+__author__ = "Diamond Light Source - Scientific Software"
+__email__ = "scientificsoftware@diamond.ac.uk"
+__version__ = "0.0.0"
+__version_tuple__ = tuple(int(x) for x in __version__.split("."))
+
 import os
 from typing import MutableSequence, Optional, Tuple
 
