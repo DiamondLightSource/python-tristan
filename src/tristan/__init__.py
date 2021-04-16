d8a14a3ecac1329ee4c07e30f5dcce88718fa3b8
Ben Williams <benjaminhwilliams@users.noreply.github.com>
2021-04-16 18:18:22 +0100

    Bump version: 0.1.1 → 0.1.2
    The image binning tools now create output NeXus files containing
    experiment metadata.

--- src/tristan/__init__.py
+++ src/tristan/__init__.py
@@ -10,7 +10,7 @@ Source.
 
 __author__ = "Diamond Light Source - Scientific Software"
 __email__ = "scientificsoftware@diamond.ac.uk"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __version_tuple__ = tuple(int(x) for x in __version__.split("."))
 
 from typing import Dict, Optional, Tuple
