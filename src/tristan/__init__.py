a5b3f0c2aa05989b11b2ac77e78054350648a843
Ben Williams <benjaminhwilliams@users.noreply.github.com>
2021-05-06 16:03:22 +0100

    Bump version: 0.1.2 → 0.1.3

--- src/tristan/__init__.py
+++ src/tristan/__init__.py
@@ -10,7 +10,7 @@ Source.
 
 __author__ = "Diamond Light Source - Scientific Software"
 __email__ = "scientificsoftware@diamond.ac.uk"
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __version_tuple__ = tuple(int(x) for x in __version__.split("."))
 
 from contextlib import ExitStack, contextmanager
