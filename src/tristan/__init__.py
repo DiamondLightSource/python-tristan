2a552bb4fbc924c4b92363c332d78723350e2c7c
Ben Williams <benjaminhwilliams@users.noreply.github.com>
2021-09-15 10:49:36 +0100

    Bump version: 0.1.5 → 0.1.6

--- src/tristan/__init__.py
+++ src/tristan/__init__.py
@@ -10,7 +10,7 @@ Source.
 
 __author__ = "Diamond Light Source - Scientific Software"
 __email__ = "scientificsoftware@diamond.ac.uk"
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 __version_tuple__ = tuple(int(x) for x in __version__.split("."))
 
 import pint
