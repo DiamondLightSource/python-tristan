ce0cd9e17d204a416c341b49f4ff95527ae0d5c3
Ben Williams <benjaminhwilliams@users.noreply.github.com>
2022-09-06 15:18:17 +0100

    Correct author and contact details

--- src/tristan/__init__.py
+++ src/tristan/__init__.py
@@ -8,8 +8,8 @@ Source.
 
 from __future__ import annotations
 
-__author__ = "Diamond Light Source - Scientific Software"
-__email__ = "scientificsoftware@diamond.ac.uk"
+__author__ = "Diamond Light Source — Data Analysis Group"
+__email__ = "dataanalysis@diamond.ac.uk"
 __version__ = "0.1.15"
 __version_tuple__ = tuple(int(x) for x in __version__.split("."))
 
