68746eddec0260264b933a11a18a0b0ace3cef3b
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2022-09-09 19:18:59 +0100

    Add parser

--- src/tristan/diagnostics/check_files.py
+++ src/tristan/diagnostics/check_files.py
@@ -1,8 +1,10 @@
 """
 Check that all modules contain valid data.
 """
+
 import argparse
 import logging
+import time
 
 from ..command_line import version_parser
 from . import diagnostics_log as log
@@ -20,11 +22,30 @@ logger = logging.getLogger("TristanDiagnostics.ModuleCheck")
 
 # Define parser
 parser = argparse.ArgumentParser(description=__doc__, parents=[version_parser])
+parser.add_argument("visitpath", type=str, help="Visit directory.")
+parser.add_argument("filename", type=str, help="Root filename.")
+parser.add_argument(
+    "-l",
+    "--list",
+    action="store_true",
+    help="Print out the list of files corresponding to each module.",
+)
+parser.add_argument(
+    "-o",
+    "--output",
+    type=str,
+    help="Output directory to save results. If not passed, the script will default to current working directory.",
+)
 
 
-def main():
+def main(args):
     log.config()
+    print(args)
 
 
 def cli():
-    main()
+    tic = time.process_time()
+    args = parser.parse_args()
+    main(args)
+    toc = time.process_time()
+    logger.debug(f"Total time taken: {toc - tic:.4f} s.")
