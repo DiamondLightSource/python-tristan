29498a935b678a3709c9f66680031758f5dd9ef4
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2022-12-09 10:26:12 +0000

    Use sphinx_rtd_theme

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,7 @@ release = ""
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx_rtd_theme",
     "sphinx.ext.autodoc",
     "sphinx.ext.coverage",
     "sphinx.ext.viewcode",
@@ -76,7 +77,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"  # "alabaster"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
